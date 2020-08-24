4d1025dc3731b011dcebb84adfafa40839c606f4
Ryan Farrow <rfarrow1010@gmail.com>
2020-08-23 23:58:19 -0400

    work begun on flesch

---
+++ flesch/script.js
@@ -0,0 +1,18 @@
+// stopped here last
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash
+
+// note: basically no reason to use "var" when declaring a variable
+// use "let" instead
+let randomNum = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+alert("I am a placeholder");
